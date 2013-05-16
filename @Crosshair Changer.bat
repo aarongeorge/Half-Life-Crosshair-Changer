ca6fa1929e21605000911368b805eb8fbcf3a4ca
Aaron George <aaron.dean.george@gmail.com>
2013-05-16 22:49:33 +1000

    Edited the batch file
    Changed the batch file so that it was better looking semantically.

--- @Crosshair Changer.bat
+++ @Crosshair Changer.bat
@@ -1,22 +1,51 @@
 @ECHO off
-ECHO ^+^+  Aaron's HL Crosshair Changer  ^+^+
-ECHO -----------------------------
-ECHO What crosshair would you like ?
-ECHO -----------------------------
-ECHO 1. Original
-ECHO 2. Default Solid Coloured
-ECHO 3. 1px
-ECHO 4. 3px
-ECHO 5. Large Circle
-ECHO 6. Medium Circle
-ECHO 7. Small Circle
-ECHO 8. Large Circle with 1px
-ECHO 9. Medium Circle with 1px
-ECHO 10. Small Circle with 1px
+ECHO ++  Aaron's HL Crosshair Changer  ++
+ECHO ------------------------------------
+ECHO    What crosshair would you like?
+ECHO ------------------------------------
+ECHO 1.  Default Crosshairs
+ECHO 2.  1px
+ECHO 3.  3px
+ECHO 4.  Large Circle
+ECHO 5.  Medium Circle
+ECHO 6.  Small Circle
+ECHO 7.  Large Circle with 1px
+ECHO 8.  Medium Circle with 1px
+ECHO 9.  Small Circle with 1px
+ECHO 10. Change back to the original crosshair
+ECHO.
+ECHO Type in the number according to the crosshair you want and press "Enter":
 SET /p crosshair=
 CLS
 GOTO CASE_%crosshair%
-	:CASE_1
+    :CASE_1
+		SET crosshair=Default
+		GOTO END_SWITCH
+    :CASE_2
+		SET crosshair=1px
+		GOTO END_SWITCH
+	:CASE_3
+		SET crosshair=3px
+		GOTO END_SWITCH
+	:CASE_4
+		SET crosshair=Large Circle
+		GOTO END_SWITCH
+	:CASE_5
+		SET crosshair=Medium Circle
+		GOTO END_SWITCH
+	:CASE_6
+		SET crosshair=Small Circle
+		GOTO END_SWITCH
+	:CASE_7
+		SET crosshair=Large Circle with 1px
+		GOTO END_SWITCH
+	:CASE_8
+		SET crosshair=Medium Circle with 1px
+		GOTO END_SWITCH
+	:CASE_9
+		SET crosshair=Small Circle with 1px
+		GOTO END_SWITCH
+	:CASE_10
 		DEL "%~dp0\valve\sprites\crosshairs.spr"
 		COPY "%~dp0\crosshairs\original.spr" "%~dp0\valve\sprites\"
 		REN "%~dp0\valve\sprites\original.spr" "crosshairs.spr"
@@ -24,37 +53,11 @@ GOTO CASE_%crosshair%
 		ECHO All done :)
 		PAUSE
 		EXIT
-    :CASE_2
-		SET crosshair=Default
-		GOTO END_SWITCH
-    :CASE_3
-		SET crosshair=1px
-		GOTO END_SWITCH
-	:CASE_4
-		SET crosshair=3px
-		GOTO END_SWITCH
-	:CASE_5
-		SET crosshair=Large Circle
-		GOTO END_SWITCH
-	:CASE_6
-		SET crosshair=Medium Circle
-		GOTO END_SWITCH
-	:CASE_7
-		SET crosshair=Small Circle
-		GOTO END_SWITCH
-	:CASE_8
-		SET crosshair=Large Circle with 1px
-		GOTO END_SWITCH
-	:CASE_9
-		SET crosshair=Medium Circle with 1px
-		GOTO END_SWITCH
-	:CASE_10
-		SET crosshair=Small Circle with 1px
-		GOTO END_SWITCH
 :END_SWITCH
-ECHO -----------------------------
-ECHO What colour would you like ?
-ECHO -----------------------------
+ECHO ++  Aaron's HL Crosshair Changer  ++
+ECHO ------------------------------------
+ECHO     What colour would you like ?
+ECHO ------------------------------------
 ECHO 1. Blue
 ECHO 2. Green
 ECHO 3. Light Blue
@@ -62,6 +65,8 @@ ECHO 4. Orange
 ECHO 5. Pink
 ECHO 6. Red
 ECHO 7. Yellow
+ECHO.
+ECHO Type in the number according to the colour you want and press "Enter":
 SET /p colour=
 CLS
 GOTO CASE_%colour%
